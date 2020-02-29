204cc229efe75378868d04dc5fb9727721393b3c
Ruchi S <ruchis2712@gmail.com>
2020-02-29 00:04:36 -0800

    Adding table structures for SQL tables and updating ETL code file

--- ETL.py
+++ ETL.py
@@ -10,7 +10,10 @@ import pandas as pd
 #####################################################################
 
 # Extract Kaggle Data
-kaggle_metadata = pd.read_csv('./US_Accidents_Dec19.tar.gz', compression='gzip', error_bad_lines=False)
+
+file_dir = 'C:/Users/ruchi/Desktop/Berkley Extension Learning Docs/Final Project'
+kaggle_metadata = pd.read_csv(f'{file_dir}/US_Accidents_Dec19.tar.gz', 
+                              compression='gzip', error_bad_lines=False, low_memory=False)
 
 ### Turn off SettingWithCopyWarning ##############
 pd.options.mode.chained_assignment = None
@@ -29,7 +32,7 @@ df_subset = kaggle_metadata[['US_Accidents_Dec19.csv','Severity','Start_Time','E
                             ]]
 
 #####################################################################
-#########  TRANSFORM  #########################################
+#########  TRANSFORM  ###############################################
 #####################################################################
 
 # Check Null Values
@@ -53,39 +56,56 @@ len(sorted_df[['Severity', 'Start_Time', 'Start_Lat', 'Start_Lng']].drop_duplica
 # Remove duplicates
 sorted_df.drop_duplicates(subset=['Severity', 'Start_Time', 'Start_Lat', 'Start_Lng'], inplace = True) 
 
+# Extract first 5 digits of zipcode where zip code is in postal format of ZIP-4
+sorted_df['Zipcode'] = sorted_df['Zipcode'].str.replace(r"-.*","")
+
 # Check length of the remaining dataset after removing duplicate and Null value rows
 len(sorted_df)
 
 # check datatypes for corrections
 sorted_df.dtypes
 
-########################################################################################
-### Split Date and Time Column into Date, Time, Time in Seconds, and Day of week columns
-########################################################################################
-
-# new data frame with split value Start date time column
-newstart = sorted_df["Start_Time"].str.split(" ",expand = True) 
-  
-# making separate Start Time column from new data frame 
-sorted_df["Start_Time_of_Day"]= newstart[1] 
-
-# new data frame with split value End date time column
-newend = sorted_df["End_Time"].str.split(" ",expand = True) 
-
-# making separate Start Time column from new data frame 
-sorted_df["End_Time_of_Day"]= newend[1] 
-
-# Convert Time to seconds for Start Time and End Time
-sorted_df['Start_seconds'] = pd.to_timedelta(sorted_df['Start_Time_of_Day']).dt.seconds
-
-# Convert Time to seconds for Start Time and End Time
-sorted_df['End_seconds'] = pd.to_timedelta(sorted_df['End_Time_of_Day']).dt.seconds
-
+# Change datatype for Start and End Time columns
 sorted_df['Start_Time'] = pd.to_datetime(sorted_df.Start_Time)
 sorted_df['End_Time'] = pd.to_datetime(sorted_df.End_Time)
 
-# Get Day of the week for the accident
-sorted_df['Day_of_Week'] = sorted_df['Start_Time'].dt.weekday
+#########################   Create Highway and Coordinates Column   ######################
+
+
+searchfor = ['highway', 'Tollway', 'expy', 'fwy', 'hwy', 'Interstate', 
+             'Tpke', 'Pkwy', 'Parkway', '-', 'US', 'Route', 
+             'FM', 'Byp', 'Trwy', 'Beltway', 'Skyway', 'Skwy', ]
+sorted_df.loc[sorted_df['Street'].str.contains('|'.join(searchfor), case=False), 'Highway'] = 'Y'
+
+# Fill NA with zero values for Highway column
+sorted_df["Highway"].fillna('N', inplace = True) 
+
+# Create Coordinates column
+sorted_df['Coordinates'] = sorted_df['Start_Lat'].map(str) + ', ' + sorted_df['Start_Lng'].map(str)
+
+##################  Prep Data and Create Dataframes for different SQL tables #############
+
+# Rename columns
+sorted_df = sorted_df.rename(index=str,columns={'US_Accidents_Dec19.csv': 'Accident_ID'})
+
+# Create Dataframes for Loading into SQL Tables 
+
+table1_df = sorted_df[['Accident_ID','Severity','Start_Time','End_Time',
+                             'Start_Lat','Start_Lng','Coordinates', 'Distance(mi)', 'Side', 
+                             'Temperature(F)','Humidity(%)','Pressure(in)',
+                             'Visibility(mi)','Wind_Direction','Wind_Speed(mph)','Precipitation(in)',
+                             'Weather_Condition','Amenity','Crossing','Junction','Railway',
+                             'Station','Stop','Traffic_Signal','Civil_Twilight'
+                             ]]
+table2_df = sorted_df[['Coordinates', 'Street','City','County','State','Zipcode',
+                       'Timezone', 'Highway']]
+
+# Drop DUPLICATE rows from Table 2
+table2_df.drop_duplicates(inplace=True)
+
+# Set Index
+table1_df.set_index('Accident_ID', inplace=True)
+table2_df.set_index('Coordinates', inplace=True)
 
 ########################################################################################
 ##############  LOAD  ##################################################################
